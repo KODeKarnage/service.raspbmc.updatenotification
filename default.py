f958ce8312a857ac4f02b6fe02e7ad31c985da57
KODeKarnage <sublime.kev@gmail.com>
2013-11-08 08:35:42 +1100

    extended logging

--- default.py
+++ default.py
@@ -26,35 +26,42 @@ import urllib2
 import random
 import datetime
 
-__script_id__  = "service.raspbmc.updatenotification"
-__addon__      = xbmcaddon.Addon(id=__script_id__)
-__scriptPath__ = __addon__.getAddonInfo('path')
-__image_file__ = os.path.join(__scriptPath__,'resources','media','RaspBMC_UA.png')
-__addon__.setSetting(id='Update_Available',value='false')
 
-def log(label, message):
-    #if settings['debug']:
-    xbmc.log(msg='RaspBMC_Notify: ' + str(label) + ' - ' + str(message))
+
+
 
 
 class Main():
 
     def __init__(self):
         xbmc.sleep(60000)
+
+        self.__script_id__  = "service.raspbmc.updatenotification"
+        self.__addon__      = xbmcaddon.Addon(id=self.__script_id__)
+        self.__scriptPath__ = self.__addon__.getAddonInfo('path')
+        self.__image_file__ = os.path.join(self.__scriptPath__,'resources','media','RaspBMC_UA.png')
+        self.__addon__.setSetting(id='Update_Available',value='false')
+
         self.window = xbmcgui.Window(10000)
         self.window.setProperty('RUA_notification','false')
+
         self.ex_version_sources =   [   "http://svn.stmlabs.com/svn/raspbmc/release/update-system/kernel/kver", 
                                         "http://svn.stmlabs.com/svn/raspbmc/release/update-system/xbmc-svcmgmt/svcver",
                                         "http://svn.stmlabs.com/svn/raspbmc/release/update-system/xbmc/xbmcver" ]
         self.in_version_sources =   [   "/scripts/upd_hist/kver",
                                         "/scripts/upd_hist/svcver",
                                         "/scripts/upd_hist/xbmcver" ] 
+
         self.base_time    = datetime.datetime.now()
         self.recheck_time = datetime.datetime.now()
         self.first_run    = True
+
         self.check_ver()
         self._daemon()
 
+    def log(self, label, message):
+        #if settings['debug']:
+        xbmc.log(msg='RaspBMC_Notify: ' + str(label) + ' - ' + str(message))
 
     def _daemon(self):
         while not xbmc.abortRequested:
@@ -66,12 +73,12 @@ class Main():
                 self.first_run = False
                 self.randy = (float(random.randint(750,1250))/1000.0)*86400.0
                 self.recheck_time = self.recheck_time + datetime.timedelta(seconds=self.randy)
-                log('recheck time',self.recheck_time)
+                self.log('recheck time',self.recheck_time)
                 
-                available = __addon__.getSetting('Update_Available')
+                available = self.__addon__.getSetting('Update_Available')
                 notified = self.window.getProperty('RUA_notification')
-                log('available',available)
-                log('notified',notified)
+                self.log('available',available)
+                self.log('notified',notified)
 
                 if available == 'true' and notified == 'false':
                     #posts notification if update is available and notification is not currently displayed
@@ -88,28 +95,35 @@ class Main():
     def check_ver(self):
         for x in range(3):
             self.XV = urllib2.urlopen(self.ex_version_sources[x]).read()
-            log('url_read',self.XV)
+            self.log('XV_location',self.ex_version_sources[x])
+            self.log('url_read',self.XV)
             with open(self.in_version_sources[x],'r') as f:
                 self.NV = f.read()
-            log('local_read',self.NV)
+            self.log('NV_location',self.in_version_sources[x])
+            self.log('local_read',self.NV)
             if self.XV > self.NV:
-                __addon__.setSetting(id='Update_Available',value='true')
-
+                self.__addon__.setSetting(id='Update_Available',value='true')
                 break
             if x == 2:
-                __addon__.setSetting(id='Update_Available',value='false')
+                self.__addon__.setSetting(id='Update_Available',value='false')
 
 
     def post_notification(self):
-        log('notifying','posting notification')
-        self.RUA_image = xbmcgui.ControlImage(15, 55, 150, 50, __image_file__)
+        self.log('notifying','posting notification')
+        self.RUA_image = xbmcgui.ControlImage(15, 55, 150, 50, self.__image_file__)
         self.window.addControl(self.RUA_image)
         self.window.setProperty('RUA_notification','true')
         self.RUA_image.setVisibleCondition('!System.ScreenSaverActive')
 
+
     def takedown_notification(self):
-        self.window.removeControl(self.RUA_image)
-        self.window.setProperty('RUA_notification','false')
+        try:
+            self.window.removeControl(self.RUA_image)
+            self.window.setProperty('RUA_notification','false')
+            self.log('takedown','notification successfully taken down')
+        except:
+            pass
+
 
 if __name__ == "__main__":
     srv = Main()
